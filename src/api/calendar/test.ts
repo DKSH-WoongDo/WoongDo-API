5f480614f970edf94742b094427445f02ebc086f
yyv2190 <yb050912@naver.com>
2022-07-28 23:24:15 +0900

    test

--- src/api/calendar/test.ts
+++ src/api/calendar/test.ts
@@ -3,6 +3,8 @@ import express, { Request, Response, NextFunction } from 'express';
 import { jwtTokenType } from '../../types';
 import { jwtToken } from '../../token';
 import { sql } from '../../dbHandle';
+import { cryptoHandle } from '../../cryptoHandle';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 router.use(express.json());
@@ -18,7 +20,7 @@ router.post('/', async (req: Request, res: Response) => {
         });
     }
 
-    const {title, s_date, e_date, content, cID}: {title: string, s_date: string, e_date: string, content: string, cID: string} = Object.assign(req.body, req.query);
+    const {title, s_date, e_date, content}: {title: string, s_date: string, e_date: string, content: string} = Object.assign(req.body, req.query);
 
     if (!title || !s_date || !e_date || !content) {
         return res.json({
@@ -26,40 +28,27 @@ router.post('/', async (req: Request, res: Response) => {
             message: '입력하지 않은 값들이 있습니다.',
         });
     }
-
+    
     const { isError, returnValue }: { isError: boolean, returnValue: jwtTokenType } = await jwtToken.verifyToken(userToken);
 
     if (isError || returnValue.type !== "A") {
         return res.json({
             isError: true,
-            message: '데이터 접근 권한이 없습니다.',
+            message: '당신은 선생님이 아니잖아',
         });
     }
 
-    try {
-        const query1 = await sql(`select * from ${process.env.MYSQL_DB}.calendar from calendarID`, [cID])
-        if(Array.isArray(query1) && query1.length === 0) {
-            const query2:any = await sql(`insert into ${process.env.MYSQL_DB}.calendar VALUES(NULL, ?, ?, ?, ?, 0)`, [title, s_date, e_date, content])
-            if (query2?.affectedRows == 0) {
-                return res.json({
-                    isError: true,
-                    message: '책을 추가하지 못했습니다.'
-                });
-            }
-            return res.json({
-                isError: false,
-                message: '새로운 책을 추가했습니다.',
-            });
-        }
+    const calendarID = cryptoHandle.SHA256(uuidv4());
+    try{
+        const q: any = await sql(`INSERT INTO ${process.env.MYSQL_DB}.calendar VALUES(?, ?, ?, ?, ?, ?)`,
+        [calendarID, returnValue.id, returnValue.name, title, s_date, e_date, content]);
         return res.json({
-            isError: true,
-            message: '이미 있는 책입니다.'
-        })
-    } catch (err) {
-        return res.json({
-            isError: true,
-            message: err
-        })
+            isError: false,
+            message: '성공적으로 일정을 수정했습니다'
+        });
+    }
+    catch(err: any) {
+        return res.json({isError});
     }
 });
 
