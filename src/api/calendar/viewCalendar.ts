e428328f98e3e28b9aa4374a7eda401ec4c3d272
kitae0522 <kitae040522@gmail.com>
2022-08-12 23:15:37 +0900

    fix bug

--- src/api/calendar/viewCalendar.ts
+++ src/api/calendar/viewCalendar.ts
@@ -10,11 +10,12 @@ router.get('/', async (req: Request, res: Response) => {
     const { targetDate, userID }: { targetDate: string, userID: string | null } = Object.assign(req.body, req.query);
 
     try {
-        const WHERE_CONDITION_USER_ID = userID ? `userID=${userID}` : '';
-        const WHERE_CONDITION_TARGET_DATE = targetDate ? `DATE_FORMAT(sDate, "%Y-%m")=${targetDate}` : '';
-        const AND_CONDITION = userID && targetDate ? 'AND' : '';
+        const WHERE_CONDITION = targetDate || userID ? 'WHERE' : '';
+        const WHERE_CONDITION_TARGET_DATE = targetDate ? `DATE_FORMAT(sDate, "%Y-%m")='${targetDate}'` : '';
+        const WHERE_CONDITION_USER_ID = userID ? `userID='${userID}'` : '';
+        const AND_CONDITION = targetDate && userID ? 'AND' : '';
 
-        const rows = await sql(`SELECT * FROM ${process.env.MYSQL_DB}.calendar WHERE ${WHERE_CONDITION_USER_ID} ${AND_CONDITION} ${WHERE_CONDITION_TARGET_DATE}`, []);
+        const rows = await sql(`SELECT * FROM ${process.env.MYSQL_DB}.calendar ${WHERE_CONDITION} ${WHERE_CONDITION_TARGET_DATE} ${AND_CONDITION} ${WHERE_CONDITION_USER_ID}`, []);
 
         return res.json({
             isError: false,
