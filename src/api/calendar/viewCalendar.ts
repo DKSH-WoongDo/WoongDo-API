b9293836a183be90255e4d05dfae7f660cd9ef38
kitae0522 <kitae040522@gmail.com>
2022-08-12 23:05:34 +0900

    사용자별 일정 확인기능 추가

--- src/api/calendar/viewCalendar.ts
+++ src/api/calendar/viewCalendar.ts
@@ -7,17 +7,18 @@ router.use(express.urlencoded({ extended: false }));
 
 router.get('/', async (req: Request, res: Response) => {
 
-    const { targetDate }: { targetDate: string } = Object.assign(req.body, req.query);
-
-    if (!targetDate) {
-        return res.json({
-            isError: true,
-            message: '입력하지 않은 값들이 있습니다.',
-        });
-    }
+    const { targetDate, userID }: { targetDate: string, userID: string | null } = Object.assign(req.body, req.query);
 
     try {
-        const rows = await sql(`SELECT * FROM ${process.env.MYSQL_DB}.calendar WHERE DATE_FORMAT(sDate, '%Y-%m') = ?`, [targetDate]);
+        const WHERE_CONDITION_USER_ID = userID ? { SYNTAX: 'userID=?', VALUE: userID } : {};
+        const WHERE_CONDITION_TARGET_DATE = targetDate ? { SYNTAX: 'DATE_FORMAT(sDate, "%Y-%m")=?', VALUE: targetDate } : {};
+        const AND_CONDITION = userID && targetDate ? 'AND' : '';
+
+        const rows = await sql(
+            `SELECT * FROM ${process.env.MYSQL_DB}.calendar WHERE ${WHERE_CONDITION_USER_ID.SYNTAX} ${AND_CONDITION} ${WHERE_CONDITION_TARGET_DATE.SYNTAX}`,
+            [WHERE_CONDITION_USER_ID.VALUE, WHERE_CONDITION_TARGET_DATE.VALUE]
+        );
+
         return res.json({
             isError: false,
             message: '일정을 로드하는데 성공했습니다.',
